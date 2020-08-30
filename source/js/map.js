81ea9c37a47748fd1854b8697230ce58b50cd1c2
wurelcy <wurelcy@yandex.ru>
2020-08-30 15:21:58 +0300

    pp

--- source/js/map.js
+++ source/js/map.js
@@ -3,6 +3,6 @@ var mapIframe = document.querySelector(".map__iframe");
 
 mapImg.addEventListener("click", function (evt) {
   evt.preventDefault();
-  mapImg.classList.remove("map--show");
-  mapIframe.classList.add("map--show");
+  mapImg.classList.remove("map__image--show");
+  mapIframe.classList.add("map__iframe--show");
 });
