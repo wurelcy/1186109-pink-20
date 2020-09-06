0ad423029da1a8ba885c49b07250734270646a1f
wurelcy <wurelcy@yandex.ru>
2020-09-06 12:32:10 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
